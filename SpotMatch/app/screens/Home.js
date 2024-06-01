68d72f01b241fe801fe1e73d271667f653d88683
Charlene <charlenecyteoh@gmail.com>
2024-06-01 17:31:24 +0800

    sign up page and name

--- SpotMatch/app/screens/Home.js
+++ SpotMatch/app/screens/Home.js
@@ -1,14 +1,33 @@
 import { StyleSheet, Text, View } from 'react-native';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Button from '../../components/navigation/Button';
-import { NavigationContainer, useNavigation } from '@react-navigation/core';
-
+import { useNavigation } from '@react-navigation/core';
+import { FIREBASE_AUTH, usersRef } from '../../firebase'; 
+import { getDocs, query, where } from 'firebase/firestore';
+import { HelloWave } from '@/components/HelloWave';
 
 const Home = () => {
   const navigation= useNavigation();
+  const [name, setName] = useState('');
+
+  const fetchName = async () => {
+    const q = query(usersRef, where('userId', '==', FIREBASE_AUTH.currentUser.uid));
+    const qSnapShot = await getDocs(q);
+    qSnapShot.forEach(doc => {
+      setName(doc.data().firstName)
+    })
+  }
+
+  useEffect(() => {
+    fetchName();
+  }, [])
+
   return (
-    <View style={{flex: 1, justifyContent:'center', alignItems: 'center'}}>
-      <Text>Home Screen</Text>
+    <View style={styles.container}>
+      <View style={styles.greeting}> 
+        <HelloWave />
+        <Text style={styles.gText}> Hello, <Text style={styles.gName}>{name} </Text> </Text>
+      </View>
       <Button type='secondary' size='s' text='Settings' onPress={() => navigation.navigate('Settings')} />
     </View>
   );
@@ -17,5 +36,27 @@ const Home = () => {
 export default Home
 
 const styles = StyleSheet.create({
+  container : {
+    flex: 1,
+    justifyContent:'space-between', 
+    alignItems: 'center',
+  }, 
+  
+  greeting: {
+    flex: 1, 
+    flexDirection: 'row', 
+    alignItems: 'center',
+    marginTop: 60,
+  }, 
+
+  gText: {
+    fontSize: 25, 
+    fontFamily: 'verdana',
+  }, 
+
+  gName: {
+    color: '#212e37',
+    fontWeight: '500', 
+  }
 
 })
--- SpotMatch/app/screens/Login.js
+++ SpotMatch/app/screens/Login.js
@@ -1,30 +1,15 @@
 import { Text, TextInput, Image, View, StyleSheet, KeyboardAvoidingView } from 'react-native';
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import Button from '../../components/navigation/Button';
-import {FIREBASE_AUTH} from '../../firebase';
-import { createUserWithEmailAndPassword , signInWithEmailAndPassword} from 'firebase/auth';
+import { FIREBASE_AUTH } from '../../firebase';
+import { signInWithEmailAndPassword} from 'firebase/auth';
+import { useNavigation } from '@react-navigation/core';
 
 const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [loading, setLoading] = useState(false);
   const auth = FIREBASE_AUTH;
-
-  
-
-  const handleSignUp = async () => {
-    setLoading(true);
-    try {
-      const response = await createUserWithEmailAndPassword(auth, email, password);
-      console.log(response);  
-      alert('Check your emails!')
-    } catch (error) {
-      console.log(error);
-      alert('Sign Up failed: ' + error.message);
-    } finally {
-      setLoading(false);
-    }
-  }
 
   const handleLogin = async () => {
     setLoading(true);
@@ -39,36 +24,43 @@ const Login = () => {
     }
     
   }
-  
+  const navigation= useNavigation();
+
   return (
     <KeyboardAvoidingView
       style={styles.container}
       behavior='padding'
     >
       <Image source={require('../../assets/images/SpotMatch-login.png')} />
-      <View style= {styles.inputContainer}>
-        <TextInput
-          value={email}
-          onChangeText={text =>setEmail(text) }
-          placeholder="Email"
-          style={styles.input}
-        /> 
+        <View style ={{alignItems: 'left', width: '100%'}}>
+          <Text style={styles.signInText}>Sign In</Text>
+        </View>
+        <View style= {styles.inputContainer}>
+          <TextInput
+            value={email}
+            onChangeText={text =>setEmail(text) }
+            placeholder="Email"
+            style={styles.input}
+          /> 
 
-        <TextInput
-          value={password}
-          onChangeText={text => setPassword(text) }
-          placeholder="Password"
-          style={styles.input}
-          secureTextEntry
-        /> 
+          <TextInput
+            value={password}
+            onChangeText={text => setPassword(text) }
+            placeholder="Password"
+            style={styles.input}
+            secureTextEntry
+          /> 
         </View>
 
-        {/* button code goes here */}
+          {/* button code goes here */}
         <View style= {styles.buttonContainer}>
           <Button type='primary' size='m' text='Login' onPress={handleLogin} />
-            <Text style= {{fontSize: 15, marginTop: 12, fontWeight: '400', color:'gray'}}>New here?</Text>
-            <Button type='secondary' size='m' text='Sign Up' onPress={handleSignUp} />
         </View>
+        <View style = {{ flex: 1, marginTop: 50, alignItems: 'center', }}> 
+          <Text style= {{fontSize: 16, fontWeight: '400', color:'gray', marginBottom: 8,}}>New here?  </Text>
+          <Button type= 'secondary' size='m' text='Create Account' onPress={() => navigation.navigate('Registration')} /> 
+        </View>
+       
     </KeyboardAvoidingView>
   );
 };
@@ -78,24 +70,33 @@ export default Login;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
+    justifyContent: "flex-start",
     alignItems: "center",
+    marginTop: 20
 
   },
 
   inputContainer: {
     width: '80%',
+    justifyContent: 'center',
+    alignItems: 'center'
+    
 
   },
 
   input: {
+    width: '100%',
     backgroundColor: 'white',
     paddingHorizontal: 15,
     paddingVertical: 10,
     borderRadius: 15,
-    marginTop: 10,
+    marginTop: 15,
     borderWidth: 1,
     borderColor: '#accafc',
+    shadowOffset: {width: 5, height: 8},  
+    shadowColor: '#171717',  
+    shadowOpacity: 0.2,  
+    shadowRadius: 7,
   },
 
   buttonContainer: {
@@ -104,6 +105,15 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     marginTop: 40,
 
+  },
+
+  signInText: {  
+    fontSize: 20,      
+    fontWeight: '400', 
+    fontFamily: 'Verdana',
+    color: '#212e37',      
+    marginBottom: 8,
+    paddingHorizontal: 42   
   },
 
 
