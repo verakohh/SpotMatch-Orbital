a8e3c01dcafe5f4045bd00baa296ddc9d8884b69
Charlene <charlenecyteoh@gmail.com>
2024-06-02 16:42:32 +0800

    xcode

---
+++ SpotMatch1/SpotMatch1/SpotMatch1App.swift
@@ -0,0 +1,17 @@
+//
+//  SpotMatch1App.swift
+//  SpotMatch1
+//
+//  Created by Charlene on 2/6/24.
+//
+
+import SwiftUI
+
+@main
+struct SpotMatch1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
