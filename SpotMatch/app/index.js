59b2c5191c0aaf329cc7d0711e03b41728c08cac
verakohhh <vrkwjacad@gmail.com>
2024-06-21 23:12:46 +0800

    Add navigation tab implementation

--- SpotMatch/app/index.js
+++ SpotMatch/app/index.js
@@ -1,54 +1,57 @@
 import 'react-native-gesture-handler';
 import * as React from 'react';
+import { View, Text, TouchableOpacity } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Registration from './screens/Registration'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Registration from './screens/Registration';
 import Login from './screens/Login';
 import Home from './screens/Home';
 import Settings from './screens/Settings/Settings';
-import { useState } from 'react';
+import MatchScreen from './screens/MatchScreen';
+import DiscoverScreen from './screens/DiscoverScreen';
+import ChatScreen from './screens/ChatScreen';
+import EventsScreen from './screens/EventsScreen';
+import NavigationTab from './screens/NavigationTab';
+import { useState, useEffect } from 'react';
 import { onAuthStateChanged } from 'firebase/auth';
-import { useEffect } from 'react';
 import { FIREBASE_AUTH } from '@/firebase';
 
 const Stack = createNativeStackNavigator();
-const InsideStack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 
 function InsideLayout() {
-  console.log("Rendering InsideLayout Navigator");
-
   return (
-    <InsideStack.Navigator initialRouteName='Home'>
-      <InsideStack.Screen name='Home' component={Home} options={{ headerShown: false }} />
-      <InsideStack.Screen name='Settings' component={Settings} />
-    </InsideStack.Navigator>
+    <Tab.Navigator tabBar={props => <NavigationTab {...props} />}>
+      <Tab.Screen name="Match" component={MatchScreen} />
+      <Tab.Screen name="Discover" component={DiscoverScreen} />
+      <Tab.Screen name="Chat" component={ChatScreen} />
+      <Tab.Screen name="Events" component={EventsScreen} />
+    </Tab.Navigator>
   );
 }
 
 export default function App() {
-
   const [user, setUser] = useState(null);
 
   useEffect(() => {
-    onAuthStateChanged(FIREBASE_AUTH, user=> {
+    const unsubscribe = onAuthStateChanged(FIREBASE_AUTH, user => {
       setUser(user);
     });
-  }, [])
+    return () => unsubscribe();
+  }, []);
 
-    if(!user) {
-      return (
-        <Stack.Navigator>
-          <Stack.Screen name="Login" component={Login} options={{ headerShown: false }}/>
-          <Stack.Screen name="Registration" component={Registration} options={{ headerShown: false }}/>
-          
-        </Stack.Navigator>
-      );
-    }
-
-    return (
+  return (
       <Stack.Navigator>
-        <Stack.Screen name="InsideLayout" component={InsideLayout} options={{ headerShown: false }}/>        
+        {user ? (
+          <Stack.Screen name="InsideLayout" component={InsideLayout} options={{ headerShown: false }} />
+        ) : (
+          <>
+            <Stack.Screen name="Login" component={Login} options={{ headerShown: false }} />
+            <Stack.Screen name="Registration" component={Registration} options={{ headerShown: false }} />
+          </>
+        )}
       </Stack.Navigator>
-    );
-  
+    
+  );
 }
