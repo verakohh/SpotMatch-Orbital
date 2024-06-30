3c1d4041adfd8f6f35fa12548aec6feda377d90e
Charlene <charlenecyteoh@gmail.com>
2024-06-30 21:15:37 +0800

    fix logout

--- SpotMatch/app/screens/SideBarScreen/LogoutScreen.js
+++ SpotMatch/app/screens/SideBarScreen/LogoutScreen.js
@@ -3,15 +3,27 @@ import React from 'react'
 import { FIREBASE_AUTH } from '@/firebase'
 import { useState } from 'react'
 import Button from '@/components/navigation/Button'
+import { useNavigation } from '@react-navigation/core';
+import { getAuth, signOut } from "firebase/auth";
+import { removeUser } from '../../User'
+
 
 export default function LogoutScreen() {
     const auth = FIREBASE_AUTH;
     const [loading, setLoading] = useState(false);
+    const navigation= useNavigation();
 
-    const handleSignOut = () => {
+
+    const handleSignOut = async () => {
         setLoading(true);
         try {
-            auth.signOut();
+            await signOut(auth).then(() => {
+                removeUser(); 
+                navigation.navigate('Login');
+            });
+            
+            // navigation.navigate('Login');
+            // removeUser();
         } catch (error) {
             console.log(error);
             alert('Sign Out failed: ' + error.message);
