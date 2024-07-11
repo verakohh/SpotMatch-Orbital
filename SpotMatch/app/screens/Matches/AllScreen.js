03abbca8a7bc13736464bca66ef1bbc71f6925c0
Charlene <charlenecyteoh@gmail.com>
2024-07-11 16:11:59 +0800

    tapCard and common box on card

---
+++ SpotMatch/app/screens/Matches/AllScreen.js
@@ -0,0 +1,275 @@
+import React, { useEffect, useState } from 'react';
+import { View, Text, TouchableOpacity, ImageBackground, Image, StyleSheet, Settings, ActivityIndicator, Dimensions, Modal, ScrollView } from 'react-native';
+import { useNavigation , useFocusEffect} from '@react-navigation/core';
+import Swiper from 'react-native-deck-swiper';
+import { getUser, getToken } from '../../User';
+import { getDoc, getDocs, updateDoc, arrayUnion, arrayRemove, where, query } from 'firebase/firestore';
+import { ref, usersColRef } from '../../../firebase';
+import { BlurView } from 'expo-blur';
+
+const AllScreen = () => {
+
+    const [userDocs, setUserDocs] = useState(null);
+    const [currentDocs, setCurrentDocs] = useState([]);
+    const [swipedUsers, setSwipedUsers] = useState([]);
+    const [dismissedUsers, setDismissedUsers] = useState([]);
+    const [loading, setLoading] = useState(true);
+   
+    const fetchData = async () => {
+        const token = await getToken();
+        const user = await getUser();
+        const userDocRef = ref(user.email);
+        const userDocSnap = await getDoc(userDocRef);
+        setLoading(true);
+    
+        console.log("at get spotifydata useEffect")
+    
+        if (userDocSnap.exists()) {
+
+            const userData = userDocSnap.data();
+            const isMatched = userData.matched || [];
+            const sentRequest = userData.sentRequest || [];
+            const dismissed = userData.dismissed || [];
+
+            setDismissedUsers(dismissed);
+            
+            const q = query(usersColRef, where("email", "!=", user.email));
+            const querySnapshot = await getDocs(q);
+
+            const filteredDocs = querySnapshot.docs.filter(doc => {
+                const docRefPath = doc.ref.path;
+                // console.log(isMatched.some(ref => ref.path === docRefPath));
+                return !(isMatched.some(ref => ref.path === docRefPath)) 
+                    && !(sentRequest.some(ref => ref.path === docRefPath))
+                    && !(dismissed.some(ref => ref.path === docRefPath));
+            });
+
+            const dismissedDocs = querySnapshot.docs.filter(doc => {
+                const docRefPath = doc.ref.path;
+                return dismissed.some(ref => ref.path === docRefPath);
+            });
+
+            const newDocs = filteredDocs.map(doc => ({ id: doc.id, ...doc.data(), docRef: doc.ref }));
+            const newDismissedDocs = dismissedDocs.map(doc => ({ id: doc.id, ...doc.data(), docRef: doc.ref }));
+            setUserDocs([...newDocs, ...newDismissedDocs]);
+            setCurrentDocs([...newDocs, ...newDismissedDocs]);
+            setLoading(false);
+
+        } else {
+            alert("Error! No userDoc");
+        }
+        
+    }
+
+    useFocusEffect(
+        React.useCallback(() => {
+            fetchData();
+        }, [])
+    );
+       
+    const handleRight = async (cardIndex) => {
+        const currUser = await getUser(); 
+        const swipedUser = currentDocs[cardIndex];
+    
+        if (swipedUser && swipedUser.docRef) {
+            try {
+                await updateDoc(swipedUser.docRef, {
+                                requestedBy: arrayUnion(currUser.docRef)
+                                
+                });
+                await updateDoc(currUser.docRef, {
+                    sentRequest: arrayUnion(swipedUser.docRef),
+                    dismissed: arrayRemove(swipedUser.docRef)
+                    
+                });
+                setSwipedUsers([...swipedUsers, swipedUser]);
+                // setCurrentDocs(currentDocs.slice(cardIndex));
+                // setRequest(arrayUnion(swipedUser.docRef));
+                console.log("Request updated successfully");
+            } catch (error) {
+                console.error("Error updating request: ", error);
+            }
+        } else {
+            console.error("No user docRef or invalid cardIndex");
+        }
+    };
+
+    const handleLeft = async (cardIndex) => {
+        const currUser = await getUser();
+        const swipedUser = currentDocs[cardIndex];
+
+        if (swipedUser && swipedUser.docRef) {
+            try {
+                await updateDoc(currUser.docRef, {
+                    dismissed: arrayUnion(swipedUser.docRef)
+                });
+                setDismissedUsers([...dismissedUsers, swipedUser]);
+                // setCurrentDocs(currentDocs.slice(cardIndex));
+                console.log(currentDocs.map(doc => doc.firstName))
+                console.log("User dismissed successfully");
+            } catch (error) {
+                console.error("Error dismissing user: ", error);
+            }
+        } else {
+            console.error("No user docRef or invalid cardIndex");
+        }
+    };
+
+
+
+    if (loading) {
+        return (
+            <View style={styles.container}>
+                <ActivityIndicator size="large" color="#0000ff" />
+            </View>
+        );
+    }
+
+    if (!userDocs || userDocs.length === 0) {
+        return (
+            <View style={styles.container}>
+                <Text>No users found.</Text>
+            </View>
+        );
+    }
+
+
+    const top3artists = card => {
+       const sliced = card.topArtists.slice(0,3);
+       return sliced.toString();
+    }
+
+    const renderCard = (card) => (
+        <View style={styles.card}>
+            <ImageBackground
+                style={styles.imgBackground}
+                resizeMode="cover"
+                source={{ uri: card.imageUrl }}
+            >
+                <BlurView
+                    intensity={150}
+                    style={[styles.nonBlurredContent, StyleSheet.absoluteFill]}
+                >
+
+                        <Image
+                            style={styles.cardImg}
+                            source={{ uri: card.imageUrl }}
+                        />
+                        <Text style={styles.title}>{card.firstName}</Text>
+                        <View style={styles.textContainer}>
+                            <Text style={styles.headerText}>Age: <Text style={styles.text}>{card.age ? card.age : "N/A"}</Text></Text>
+                            <Text style={styles.headerText}>Top 3 Artists: <Text style={styles.text}>{card.topArtists ? top3artists(card) : "N/A"}</Text></Text>
+                            <Text style={styles.headerText}>Top Track:  
+                            {card.topTracks && card.topTracks.slice(0, 1).map((track, index) => (
+                                <Text key={index} style={styles.text}> {track.name} by {track.artist} </Text>
+                            ))}
+                            </Text>
+                            <Text style={styles.headerText}>Top 3 Genres: <Text style={styles.text}>{card.genres ? card.genres.slice(0, 3).toString() : "N/A"}</Text></Text>
+                        </View>
+
+                </BlurView>
+            </ImageBackground>
+        </View>
+    );
+
+    return (
+        <View style={styles.container}>
+            <Swiper
+                cards={currentDocs}
+                renderCard={renderCard}
+                onSwipedRight={handleRight}
+                onSwipedLeft={handleLeft}
+                stackSize={3}
+                stackSeparation={15}
+                disableTopSwipe
+                disableBottomSwipe
+                backgroundColor={"transparent"}
+                onSwipedAll={fetchData}
+            />
+        </View>
+    );
+}
+
+export default AllScreen;
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: '#FAF4EC',
+        
+    },
+    imgBackground: {
+        width: "100%",
+        height: "100%",
+        flex: 1,
+      },
+    card: {
+        flex: Dimensions.get("window").height < 700 ? 0.5 : 0.6,
+        borderRadius: 8,
+        shadowRadius: 25,
+        shadowColor: '#171717',  
+        shadowOpacity: 0.2,
+        shadowOffset: { width: 0, height: 0 },
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: 'lightgrey',
+        overflow: "hidden",
+      },
+    cardImg: {
+        flex: 1,
+        width: "70%",
+        height: 220,
+        resizeMode: "contain",
+        borderRadius: 8,
+        marginTop: 15,
+        marginbottom: 8,
+        marginLeft: '15%',
+    },
+    title: {
+        marginTop: 2,
+        fontSize: 25,
+        color: "white",
+        width: "100%",
+        textShadowColor: "#171717",
+        textShadowOffset: { width: 0, height: 5 },
+        textShadowRadius: 8,
+        padding: 10,
+        textAlign: "center",
+    },
+    text: {
+        color: "white",
+        fontSize: 16,
+        fontWeight: "400",
+        textShadowColor: "#171717",
+        textShadowOffset: { width: 0, height: 2 },
+        textShadowRadius: 6,
+        shadowOpacity: 0.2,
+    },
+    headerText: {
+        color: "white",
+        textAlign: "left",
+        fontSize: 18,
+        fontWeight: "600",
+        marginBottom: 7,
+        marginLeft: 8,
+        textShadowColor: "#171717",
+        textShadowOffset: { width: 0, height: 2 },
+        textShadowRadius: 4,
+        shadowOpacity: 0.2,
+
+        
+    },
+    textContainer: {
+        color: "white",
+        textAlign: "left",
+        fontSize: 18,
+        margin: 15,
+        fontWeight: "600",
+    },
+    scrollViewContent: {
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+});
