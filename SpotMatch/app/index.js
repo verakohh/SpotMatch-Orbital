e9fcb55a1f757e6873f3d1ee12343803eb82c5f8
verakohhh <vrkwjacad@gmail.com>
2024-07-22 00:36:46 +0800

    updated working UI

--- SpotMatch/app/index.js
+++ SpotMatch/app/index.js
@@ -1,14 +1,79 @@
+// import 'react-native-gesture-handler';
+// import * as React from 'react';
+// import { useState } from 'react';
+// import { NavigationContainer } from '@react-navigation/native';
+// import { createNativeStackNavigator } from '@react-navigation/native-stack';
+// import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+// import { UserContext } from './UserContext';
+// import Registration from './screens/Registration';
+// import Login from './screens/Login';
+// import SideBar from './screens/SideBar';
+// import Access from './screens/Access';
+// import MatchesProfileScreen from './screens/SideBarScreen/MatchesProfileScreen';
+// import ActivitiesScreen from './screens/Events/ActivitiesScreen';
+// import ActivityDetails from './screens/Events/ActivityDetails';
+// import ChatListScreen from './screens/ChatListScreen';
+// import ChatScreen from './screens/ChatScreen';
+// import MatchScreen from './screens/MatchScreen';
+// import DiscoverScreen from './screens/DiscoverScreen';
+// import EventsScreen from './screens/EventsScreen';
+// import NavigationTab from './screens/NavigationTab';
+// import ChatMusicScreen from './screens/ChatMusicScreen';
+// import { ChatProvider } from './screens/context/ChatContext';
+
+// const Stack = createNativeStackNavigator();
+// const Tab = createBottomTabNavigator();
+
+// function MainTabs() {
+//   return (
+//     <Tab.Navigator tabBar={(props) => <NavigationTab {...props} />}>
+//       <Tab.Screen name="Match" component={MatchScreen} />
+//       <Tab.Screen name="Discover" component={DiscoverScreen} />
+//       <Tab.Screen name="Chat" component={ChatListScreen} /> {/* Ensure correct screen name */}
+//       <Tab.Screen name="Events" component={EventsScreen} />
+//     </Tab.Navigator>
+//   );
+// }
+
+// export default function App() {
+//   const [user, setUser] = useState({});
+//   const [token, setToken] = useState("");
+
+//   return (
+//     <ChatProvider>
+//       <UserContext.Provider value={{ user, setUser, token, setToken }}>
+//           <Stack.Navigator initialRouteName="Access">
+//             <Stack.Screen name="Access" component={Access} options={{ headerShown: false }} />
+//             <Stack.Screen name="Login" component={Login} options={{ headerShown: false }} />
+//             <Stack.Screen name="Registration" component={Registration} options={{ headerShown: false }} />
+//             <Stack.Screen name="SideBar" component={SideBar} options={{ headerShown: false }} />
+//             <Stack.Screen name="Activities" component={ActivitiesScreen} options={{ headerShown: false }} />
+//             <Stack.Screen name="ActivityDetails" component={ActivityDetails} options={{ headerShown: false }} />
+//             <Stack.Screen name="MainTabs" component={MainTabs} options={{ headerShown: false }} />
+//             <Stack.Screen name="ChatScreen" component={ChatScreen} options={{ headerShown: false }} />
+//             <Stack.Screen name="ChatMusicScreen" component={ChatMusicScreen} options={{ headerShown: false }} />
+//             <Stack.Screen name="MatchesProfileScreen" component={MatchesProfileScreen} options={{ headerShown: false }} />
+//           </Stack.Navigator>
+//       </UserContext.Provider>
+//     </ChatProvider>
+//   );
+// }
+// App.js
+
 import 'react-native-gesture-handler';
 import * as React from 'react';
-import { useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { UserContext } from './UserContext';
+import { UserProvider } from './UserContext';
+import { ChatProvider } from './screens/context/ChatContext';
 import Registration from './screens/Registration';
 import Login from './screens/Login';
 import SideBar from './screens/SideBar';
-import Access from './screens/Access';
+import SignUpScreen from './screens/SignUp/SignUpScreen';
+import SignUpStep2Screen from './screens/SignUp/SignUpStep2Screen';
+import SpotifyAuthScreen from './screens/SignUp/SpotifyAuthScreen';
+import WelcomeScreen from './screens/SignUp/WelcomeScreen';
 import MatchesProfileScreen from './screens/SideBarScreen/MatchesProfileScreen';
 import ActivitiesScreen from './screens/Events/ActivitiesScreen';
 import ActivityDetails from './screens/Events/ActivityDetails';
@@ -19,7 +84,6 @@ import DiscoverScreen from './screens/DiscoverScreen';
 import EventsScreen from './screens/EventsScreen';
 import NavigationTab from './screens/NavigationTab';
 import ChatMusicScreen from './screens/ChatMusicScreen';
-import { ChatProvider } from './screens/context/ChatContext';
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
@@ -29,21 +93,21 @@ function MainTabs() {
     <Tab.Navigator tabBar={(props) => <NavigationTab {...props} />}>
       <Tab.Screen name="Match" component={MatchScreen} />
       <Tab.Screen name="Discover" component={DiscoverScreen} />
-      <Tab.Screen name="Chat" component={ChatListScreen} /> {/* Ensure correct screen name */}
+      <Tab.Screen name="Chat" component={ChatListScreen} />
       <Tab.Screen name="Events" component={EventsScreen} />
     </Tab.Navigator>
   );
 }
 
 export default function App() {
-  const [user, setUser] = useState({});
-  const [token, setToken] = useState("");
-
   return (
-    <ChatProvider>
-      <UserContext.Provider value={{ user, setUser, token, setToken }}>
-          <Stack.Navigator initialRouteName="Access">
-            <Stack.Screen name="Access" component={Access} options={{ headerShown: false }} />
+    <UserProvider>
+      <ChatProvider>
+          <Stack.Navigator initialRouteName="SignUpScreen">
+            <Stack.Screen name="SignUpScreen" component={SignUpScreen} options={{ headerShown: false }} />
+            <Stack.Screen name="SignUpStep2Screen" component={SignUpStep2Screen} options={{ headerShown: false }} />
+            <Stack.Screen name="SpotifyAuthScreen" component={SpotifyAuthScreen} options={{ headerShown: false }} />
+            <Stack.Screen name="WelcomeScreen" component={WelcomeScreen} options={{ headerShown: false }} />
             <Stack.Screen name="Login" component={Login} options={{ headerShown: false }} />
             <Stack.Screen name="Registration" component={Registration} options={{ headerShown: false }} />
             <Stack.Screen name="SideBar" component={SideBar} options={{ headerShown: false }} />
@@ -54,7 +118,7 @@ export default function App() {
             <Stack.Screen name="ChatMusicScreen" component={ChatMusicScreen} options={{ headerShown: false }} />
             <Stack.Screen name="MatchesProfileScreen" component={MatchesProfileScreen} options={{ headerShown: false }} />
           </Stack.Navigator>
-      </UserContext.Provider>
-    </ChatProvider>
+      </ChatProvider>
+    </UserProvider>
   );
 }
--- SpotMatch/app/screens/context/ChatContext.js
+++ SpotMatch/app/screens/context/ChatContext.js
@@ -1,5 +1,57 @@
+// import React, { createContext, useContext, useReducer, useState } from 'react';
+// import { FIREBASE_AUTH } from '../../../firebase'; // Ensure this path is correct
+
+// const ChatContext = createContext();
+
+// const INITIAL_STATE = {
+//   chatId: "null",
+//   user: {},
+//   messages: []
+// };
+
+// const chatReducer = (state, action) => {
+//   const currentUser = FIREBASE_AUTH.currentUser;
+//   switch (action.type) {
+//     case "CHANGE_USER":
+//       console.log("Current User ID:", currentUser.uid);
+//       console.log("Matched User ID:", action.payload.userId);
+
+//       const chatId = currentUser.uid > action.payload.userId
+//         ? `${currentUser.uid}_${action.payload.userId}`
+//         : `${action.payload.userId}_${currentUser.uid}`;
+
+//       return {
+//         ...state,
+//         user: action.payload,
+//         chatId: chatId,
+//       };
+//     case "SET_MESSAGES":
+//       return {
+//         ...state,
+//         messages: action.payload
+//       };
+//     default:
+//       return state;
+//   }
+// };
+
+// export const ChatProvider = ({ children }) => {
+//   const [state, dispatch] = useReducer(chatReducer, INITIAL_STATE);
+//   const [messages, setMessages] = useState([]);
+
+//   return (
+//     <ChatContext.Provider value={{ data: state, dispatch, messages, setMessages }}>
+//       {children}
+//     </ChatContext.Provider>
+//   );
+// };
+
+// export const useChat = () => {
+//   return useContext(ChatContext);
+// };
+
 import React, { createContext, useContext, useReducer, useState } from 'react';
-import { FIREBASE_AUTH } from '../../../firebase'; // Ensure this path is correct
+import { FIREBASE_AUTH } from '../../../firebase';
 
 const ChatContext = createContext();
 
@@ -13,9 +65,6 @@ const chatReducer = (state, action) => {
   const currentUser = FIREBASE_AUTH.currentUser;
   switch (action.type) {
     case "CHANGE_USER":
-      console.log("Current User ID:", currentUser.uid);
-      console.log("Matched User ID:", action.payload.userId);
-
       const chatId = currentUser.uid > action.payload.userId
         ? `${currentUser.uid}_${action.payload.userId}`
         : `${action.payload.userId}_${currentUser.uid}`;
@@ -46,6 +95,4 @@ export const ChatProvider = ({ children }) => {
   );
 };
 
-export const useChat = () => {
-  return useContext(ChatContext);
-};
+export const useChatContext = () => useContext(ChatContext);
