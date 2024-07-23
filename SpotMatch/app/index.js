e66c7fe4204935009ecedc2a3e4915e8c347d19a
verakohhh <vrkwjacad@gmail.com>
2024-07-24 00:01:33 +0800

    WIP: Saving current progress

--- SpotMatch/app/index.js
+++ SpotMatch/app/index.js
@@ -70,6 +70,7 @@ import { ChatProvider } from './screens/context/ChatContext';
 import Registration from './screens/Registration';
 import Login from './screens/Login';
 import SideBar from './screens/SideBar';
+import AccountManagement from './screens/SideBarScreen/AccountManagement';
 import SignUpScreen from './screens/SignUp/SignUpScreen';
 import SignUpStep2Screen from './screens/SignUp/SignUpStep2Screen';
 import SpotifyAuthScreen from './screens/SignUp/SpotifyAuthScreen';
@@ -119,6 +120,7 @@ export default function App() {
             <Stack.Screen name="ChatScreen" component={ChatScreen} options={{ headerShown: false }} />
             <Stack.Screen name="ChatMusicScreen" component={ChatMusicScreen} options={{ headerShown: false }} />
             <Stack.Screen name="MatchesProfileScreen" component={MatchesProfileScreen} options={{ headerShown: false }} />
+            <Stack.Screen name="AccountManagement" component={AccountManagement} options={{ headerShown: false }} />
           </Stack.Navigator>
       </ChatProvider>
     </UserProvider>
